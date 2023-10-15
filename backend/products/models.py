7e2d3fbb6ae0912549535cff87c7cc24d7b5a60c
JoshLarouche <joshua.t.larouche@gmail.com>
2023-10-15 11:32:21 -0700

    initial django backend api setup

---
+++ backend/products/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Product(models.Model):
+    productName = models.CharField("Product Name", max_length=240)
+    productOwnerName = models.CharField("Product Owner Name", max_length=240)
+    developers = models.CharField("Developers", max_length=240)
+    scrumMasterName = models.CharField("Scrum Master Name", max_length=240)
+    startDate = models.DateField("Start Date", auto_now_add=True)
+    methodology = models.CharField("Methodology", max_length=240)
+    location = models.CharField("Location", max_length=240)
+
+    def __str__(self):
+        return self.productName
